77b67eb60cfe89bd1989e7beb03ad05d7f73472a
HozaifAwan <149043053+HozaifAwan@users.noreply.github.com>
2026-01-10 02:45:06 -0600

    Update log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,6 +3,7 @@ def analyze_log(file_path):
     error_count = 0
     failed_count = 0
     total_lines = 0
+    
     try:
         with open(file_path, "r") as file:
             lines = file.readlines()
